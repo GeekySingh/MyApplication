32b515a17ba697a94efda15d760032b132ad00cc
NAGARRO\gaganpreetsingh <gaganpreet.singh@nagarro.com>
2018-08-29 18:27:20 +0530

    added test cases

---
+++ app/src/androidTest/java/com/example/myapplication/DateUtilsTest.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class DateUtilsTest {
+
+    @Test
+    public void dateUtilsFormat_isCorrect() throws Exception {
+        long epoc = 1446885450; //7th Nov 2015
+        Date date = DateUtils.epocSecondsToDate(epoc);
+        assertEquals("Date time in millis is wrong",
+                epoc * 1000, date.getTime());
+        String day = DateUtils.dateToDayDateString(date, true);
+        assertEquals("Day is wrong", "SAT", day);
+    }
+
+    @Test
+    public void dateUtilsFormat_anotherTest() throws Exception {
+
+    }
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("main activity");
+    }
+
+    public void printScreenName(String screenName) {
+        System.out.print(screenName);
     }
 }
