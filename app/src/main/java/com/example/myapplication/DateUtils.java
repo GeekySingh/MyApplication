7d6980bc6453e205d8c4ddd965a052b1bb2a7642
NAGARRO\gaganpreetsingh <gaganpreet.singh@nagarro.com>
2018-09-04 13:39:16 +0530

    temp test changes

--- app/src/main/java/com/example/myapplication/DateUtils.java
+++ app/src/main/java/com/example/myapplication/DateUtils.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-public class DateUtils {
+public class DateUtil {
 
     public static final SimpleDateFormat DISPLAY;
     public static final SimpleDateFormat DISPLAY_SHORT;
